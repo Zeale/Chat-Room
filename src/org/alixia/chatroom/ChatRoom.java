405e7bc5ef48eceef4b35aeead13948c8df51549
Zeale <a@infinityminers.com>
2018-02-18 23:19:28 -0600

    Moved fields into ChatRoom class to begin with

--- src/org/alixia/chatroom/ChatRoom.java
+++ src/org/alixia/chatroom/ChatRoom.java
@@ -5,31 +5,95 @@ import java.util.List;
 
 import org.alixia.chatroom.connections.Client;
 import org.alixia.chatroom.connections.Server;
+import org.alixia.chatroom.texts.BasicInfoText;
+import org.alixia.chatroom.texts.Println;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class ChatRoom {
+	private final static Background DEFAULT_NODE_BACKGROUND = new Background(
+			new BackgroundFill(new Color(0.4, 0.4, 0.4, 0.7), null, null));
 
 	private List<Client> clients = new ArrayList<>();
 	private List<Server> servers = new ArrayList<>();
 
-	private final AnchorPane root, pane;
-	private final TextFlow flow, output;
-	private final TextArea input;
+	// Some of these fields have aliases.
+	private final TextFlow flow = new TextFlow(), output = flow, console = flow;
+	private final TextArea input = new TextArea();
+	private final Button sendButton = new Button("Send");
+	private final AnchorPane root = new AnchorPane(flow, input, sendButton), pane = root;
 
+	private final Scene scene = new Scene(root);
 	private final Stage stage;
-	private final Scene scene;
 
-	public ChatRoom(AnchorPane root, TextFlow flow, TextArea input, Stage stage) throws RuntimeException {
-		this.root = pane = root;
-		this.flow = output = flow;
-		this.input = input;
+	ChatRoom(Stage stage) {
 		this.stage = stage;
-		this.scene = stage.getScene();
+
+		stage.setWidth(800);
+		stage.setHeight(600);
+		stage.show();
+
+		flow.setBackground(DEFAULT_NODE_BACKGROUND);
+		input.setBackground(DEFAULT_NODE_BACKGROUND);
+
+		AnchorPane.setLeftAnchor(flow, 50d);
+		AnchorPane.setRightAnchor(flow, 50d);
+		AnchorPane.setTopAnchor(flow, 0d);
+		AnchorPane.setBottomAnchor(flow, 250d);
+
+		AnchorPane.setBottomAnchor(input, 0d);
+		AnchorPane.setLeftAnchor(input, 0d);
+		AnchorPane.setRightAnchor(input, 0d);
+
+		AnchorPane.setRightAnchor(sendButton, 50d);
+		AnchorPane.setBottomAnchor(sendButton, 88.5);
+
+		input.setMaxHeight(200);
+
+		// Add a ScrollPane to wrap flow
+		root.setBackground(DEFAULT_NODE_BACKGROUND);
+
+		stage.setScene(scene);
+
+		try {
+			tryInit();
+		} catch (Exception e) {
+			Text error = new Text("An error occurred.");
+			error.setFont(Font.font(30));
+			error.setFill(Color.CRIMSON);
+			flow.setTextAlignment(TextAlignment.CENTER);
+			flow.getChildren().add(error);
+
+		}
+	}
+
+	private void tryInit() {
+		input.setOnKeyPressed(event -> {
+			if (event.getCode().equals(KeyCode.ENTER)) {
+				onUserSubmit();
+				event.consume();
+			}
+		});
+		sendButton.setOnAction(event -> {
+			onUserSubmit();
+			event.consume();
+		});
+
+	}
+	private void onUserSubmit() {
+		input.setText("");
 	}
 
 }
--- src/org/alixia/chatroom/Launch.java
+++ src/org/alixia/chatroom/Launch.java
@@ -1,60 +1,13 @@
 package org.alixia.chatroom;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class Launch extends Application {
 
-	private TextFlow flow = new TextFlow();
-	private TextArea input = new TextArea();
-	private final static Background DEFAULT_NODE_BACKGROUND = new Background(
-			new BackgroundFill(new Color(0.4, 0.4, 0.4, 0.7), null, null));
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setWidth(800);
-		primaryStage.setHeight(600);
-		primaryStage.show();
-
-		flow.setBackground(DEFAULT_NODE_BACKGROUND);
-		input.setBackground(DEFAULT_NODE_BACKGROUND);
-
-		AnchorPane.setLeftAnchor(flow, 50d);
-		AnchorPane.setRightAnchor(flow, 50d);
-		AnchorPane.setTopAnchor(flow, 0d);
-		AnchorPane.setBottomAnchor(flow, 250d);
-
-		AnchorPane.setBottomAnchor(input, 0d);
-		AnchorPane.setLeftAnchor(input, 0d);
-		AnchorPane.setRightAnchor(input, 0d);
-
-		input.setMaxHeight(200);
-
-		// Add a ScrollPane to wrap flow
-		AnchorPane root = new AnchorPane(flow, input);
-		root.setBackground(DEFAULT_NODE_BACKGROUND);
-
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-		try {
-			new ChatRoom(root, flow, input, primaryStage);
-		} catch (Exception e) {
-			Text error = new Text("An error occurred.");
-			error.setFont(Font.font(30));
-			error.setFill(Color.CRIMSON);
-			flow.setTextAlignment(TextAlignment.CENTER);
-			flow.getChildren().add(error);
-		}
+		new ChatRoom(primaryStage);
 	}
 
 	public static void main(String[] args) {
