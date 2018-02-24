4f4a986508fde8f4202e898b3a736bb96ba16e1c
Zeale <a@infinityminers.com>
2018-02-24 14:32:32 -0600

    Updated server thread & server class

--- src/org/alixia/chatroom/connections/Server.java
+++ src/org/alixia/chatroom/connections/Server.java
@@ -17,24 +17,30 @@ public class Server extends NamedObject {
 
 	private final ServerSocket socket;
 
-	private boolean accept = true, running = true;
+	private boolean running = true;
 
 	private Thread acceptThread = new Thread(new Runnable() {
 
 		@Override
 		public void run() {
-			while (accept && running)
+			while (running)
 				try {
 					final Socket connection = socket.accept();
 					new Thread(() -> acceptConnection(connection)).start();
 				} catch (final SocketException e) {
-					// Server terminated
+					// Server terminated. Do not make acceptThread point to a new Thread obj. This
+					// Server object is no longer usable since the socket has been closed.
+					return;
 				} catch (final IOException e) {
 					System.err.println("Failed to accept an incoming connection.");
 				}
 			acceptThread = new Thread(this);
+			acceptThread.setDaemon(true);
 		}
 	});
+	{
+		acceptThread.setDaemon(true);
+	}
 
 	public Server(String name) throws IOException {
 		super(name);
@@ -48,6 +54,12 @@ public class Server extends NamedObject {
 		acceptThread.start();
 	}
 
+	/**
+	 * This is the method that handles new connections.
+	 * 
+	 * @param connection
+	 *            The new connection.
+	 */
 	protected void acceptConnection(final Socket connection) {
 		try {
 			ServerClient client = new ServerClient(connection);
@@ -99,16 +111,25 @@ public class Server extends NamedObject {
 
 	}
 
-	public void allowIncomingConnections() {
-		final boolean accept = this.accept;
-		this.accept = true;
-		if (!accept)
-			acceptThread.start();
+	/**
+	 * Starts the thread that accepts incoming connections if it is not already
+	 * running.
+	 */
+	public void startConnectionThread() {
+		// "running" needs to be true for the thread to run.
+		running = true;
+		if (acceptThread.isAlive())
+			try {
+				acceptThread.start();
+			} catch (IllegalThreadStateException e) {
+				// Do nothing. The thread is (probably) already running
+			}
 	}
 
 	public void blockIncomingConnections() {
-		accept = false;
-
+		// The thread will notice that "running" is false and stop itself. We don't need
+		// to stop it.
+		running = false;
 	}
 
 	private List<ServerClient> connections = new LinkedList<>();
