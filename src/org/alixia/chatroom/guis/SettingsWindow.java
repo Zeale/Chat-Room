ec5e74992f999fb67c100faa08dfea43bc0a5f16
Zeale <a@infinityminers.com>
2018-03-05 10:56:04 -0600

    Styled Settings window

--- src/org/alixia/chatroom/guis/SettingsWindow.java
+++ src/org/alixia/chatroom/guis/SettingsWindow.java
@@ -1,5 +1,6 @@
 package org.alixia.chatroom.guis;
 
+import org.alixia.chatroom.ChatRoom;
 import org.alixia.chatroom.resources.fxnodes.popbutton.PopButton;
 
 import javafx.beans.value.ChangeListener;
@@ -7,6 +8,12 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -21,6 +28,13 @@ public class SettingsWindow extends Stage {
 	{
 		initStyle(StageStyle.TRANSPARENT);
 		root.setMinSize(600, 400);
+		root.setBorder(new Border(new BorderStroke(ChatRoom.DEFAULT_WINDOW_BORDER_COLOR, BorderStrokeStyle.SOLID, null,
+				new BorderWidths(2))));
+		root.setBackground(
+				new Background(new BackgroundFill(new Color(ChatRoom.DEFAULT_WINDOW_BACKGROUND_COLOR.getRed(),
+						ChatRoom.DEFAULT_WINDOW_BACKGROUND_COLOR.getGreen(),
+						ChatRoom.DEFAULT_WINDOW_BACKGROUND_COLOR.getBlue(), 1), null, null)));
+		setAlwaysOnTop(true);
 		setScene(scene);
 
 		scene.setFill(Color.TRANSPARENT);
