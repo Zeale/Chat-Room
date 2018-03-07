6740c836a16189d42abaf9bd98e4eea4882d6320
Zeale <a@infinityminers.com>
2018-03-07 17:09:26 -0600

    Added saving/loading methods to AuthServer

--- src/org/alixia/chatroom/internet/AuthServer.java
+++ src/org/alixia/chatroom/internet/AuthServer.java
@@ -1,13 +1,19 @@
 package org.alixia.chatroom.internet;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
 import java.util.UUID;
 
 import org.alixia.chatroom.internet.SessionIDPacket.Success;
@@ -24,8 +30,83 @@ public class AuthServer {
 		handler.start();
 	}
 
-	public void addUser(String username, String password) {
+	public boolean addUser(String username, String password) {
+		if (users.containsKey(username))
+			return false;
 		users.put(username, new User(username, password));
+		return true;
+	}
+
+	public void store(File path) throws FileNotFoundException {
+		try (PrintWriter writer = new PrintWriter(new FileOutputStream(path))) {
+			for (Entry<String, User> e : users.entrySet())
+				// The first colon (:) will be parsed as a separator between usernames and
+				// passwords when this file is read back into the program. The only char that we
+				// can't allow in usernames or passwords is '\n'. That char will be used to
+				// separate each user in the file.
+				//
+				// Since Windows uses \r\n isntead of \n for returns, if a person on Windows
+				// opens the file and makes an edit, such as adding a username, I believe that
+				// any return they add will be saved as \r\n instead of \n. Since both of these
+				// look the same to the user, they won't see anything wrong. This program would
+				// also read \r into the password. Because of this, no whitespace chars will be
+				// allowed inside usernames or passwords.
+				writer.println(e.getKey() + ":" + e.getValue());
+
+			writer.flush();
+		}
+
+	}
+
+	public void store(String path) throws FileNotFoundException {
+		store(new File(path));
+	}
+
+	public static Map<String, User> read(File path) throws FileNotFoundException, UserDataParseException {
+
+		Map<String, User> users = new HashMap<>();
+
+		try (Scanner scanner = new Scanner(path);) {
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if (!line.contains(":"))
+					throw new UserDataParseException(
+							"No colon separator found in line (:). A colon is used to separate the username from the password of a user. Each line has a different user's data on it. Without a colon on a line, there's no telling of what part is the username or password.",
+							line);
+				String username = line.substring(0, line.indexOf(":") + 1);
+				String password = line.substring(line.indexOf(":") + 1);
+
+				users.put(username, new User(username, password));
+
+			}
+		}
+		return users;
+	}
+
+	public void load(File path) throws FileNotFoundException, UserDataParseException {
+		this.users = read(path);
+	}
+
+	public void load(String path) throws FileNotFoundException, UserDataParseException {
+		load(new File(path));
+	}
+
+	public static final class UserDataParseException extends Exception {
+		/**
+		 * SUID
+		 */
+		private static final long serialVersionUID = 1L;
+		public final String line;
+
+		public UserDataParseException(String message, String line) {
+			super(message);
+			this.line = line;
+		}
+
+	}
+
+	public static Map<String, User> read(String path) throws FileNotFoundException, UserDataParseException {
+		return read(new File(path));
 	}
 
 	private Thread handler = new Thread(new Runnable() {
@@ -46,8 +127,9 @@ public class AuthServer {
 						System.err.println("CLOSING AUTHSERVER DUE TO REPETITIVE EXCEPTIONS...");
 					}
 				}
-
 			}
+			handler = new Thread(this);
+			handler.setDaemon(true);
 		}
 	});
 
@@ -113,6 +195,22 @@ public class AuthServer {
 	}
 
 	public static final class User implements Serializable {
+
+		public static boolean verifyUsername(String username) {
+			return verify(username);
+		}
+
+		public static boolean verifyPassword(String password) {
+			return verify(password);
+		}
+
+		private static boolean verify(String text) {
+			for (char c : text.toCharArray())
+				if (Character.isWhitespace(c))
+					return false;
+			return true;
+		}
+
 		/**
 		 * 
 		 */
@@ -120,7 +218,9 @@ public class AuthServer {
 		public final String username, password;
 		private UUID sessionID;
 
-		public User(String username, String password) {
+		public User(String username, String password) throws IllegalArgumentException {
+			if (!(verifyUsername(username) && verifyPassword(password)))
+				throw new IllegalArgumentException("Illegal username and/or password.");
 			this.username = username;
 			this.password = password;
 		}
