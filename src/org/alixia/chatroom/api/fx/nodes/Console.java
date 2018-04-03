b509e600cb4a72f2331e100298ea7883f4c3a49b
Zeale <a@infinityminers.com>
2018-04-03 01:06:08 -0500

    Updated Console class

--- src/org/alixia/chatroom/api/fx/nodes/Console.java
+++ src/org/alixia/chatroom/api/fx/nodes/Console.java
@@ -5,14 +5,44 @@ import org.alixia.chatroom.api.fx.tools.FXTools;
 import org.alixia.chatroom.api.guis.ChatRoomWindow;
 import org.alixia.chatroom.api.texts.BasicInfoText;
 
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
 
-public class Console extends TextFlow implements org.alixia.chatroom.api.Console, Printable {
+public class Console extends AnchorPane implements org.alixia.chatroom.api.Console, Printable {
+
+	private final TextFlow console = new TextFlow();
+	private final ScrollPane scrollWrapper = new ScrollPane(console);
 
 	{
-		setBackground(FXTools.getBackgroundFromColor(ChatRoomWindow.DEFAULT_NODE_OUTPUT_COLOR));
+
+		getChildren().add(scrollWrapper);
+
+		scrollWrapper.getStylesheets().add("/org/alixia/chatroom/stylesheet.css");
+		scrollWrapper.setFitToHeight(true);
+		scrollWrapper.setFitToWidth(true);
+
+		Background transparentBackground = FXTools.getBackgroundFromColor(Color.TRANSPARENT);
+		setBackground(transparentBackground);
+		scrollWrapper.setBackground(transparentBackground);
+
+		console.setBackground(FXTools.getBackgroundFromColor(ChatRoomWindow.DEFAULT_NODE_OUTPUT_COLOR));
+
+		AnchorPane.setBottomAnchor(scrollWrapper, 0d);
+		AnchorPane.setTopAnchor(scrollWrapper, 0d);
+		AnchorPane.setLeftAnchor(scrollWrapper, 0d);
+		AnchorPane.setRightAnchor(scrollWrapper, 0d);
+
+		setTextAlignment(TextAlignment.CENTER);
+
+	}
+
+	public final void setTextAlignment(TextAlignment value) {
+		console.setTextAlignment(value);
 	}
 
 	@Override
@@ -22,7 +52,7 @@ public class Console extends TextFlow implements org.alixia.chatroom.api.Console
 
 	@Override
 	public void printText(Text text) {
-		getChildren().add(text);
+		console.getChildren().add(text);
 	}
 
 }
